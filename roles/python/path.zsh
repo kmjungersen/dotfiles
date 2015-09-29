6f5e525c8b12f69afa7f3d28ab7a2192bb1b8fdc
kmjungersen <kmjungersen@gmail.com>
2015-09-29 00:30:19 -0400

    added virtualenvwrapper support

--- roles/python/path.zsh
+++ roles/python/path.zsh
@@ -5,3 +5,9 @@
 
 ## Uncomment if you use miniconda
 # export PATH=$HOME/miniconda/bin:$PATH
+
+# For VirtualenvWrapper
+
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+export WORKON_HOME=~/Envs
+source /usr/local/bin/virtualenvwrapper.sh
