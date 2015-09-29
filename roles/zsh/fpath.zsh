fcaabfec95dac0db3429d2becf5831ae6dc23ce3
kmjungersen <kmjungersen@gmail.com>
2015-09-29 00:31:35 -0400

    added docker to path

--- roles/zsh/fpath.zsh
+++ roles/zsh/fpath.zsh
@@ -1,2 +1,5 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+# Composer support, which doesn't really belong here but whatever...
+PATH=$HOME/.composer/vendor/bin:$PATH
