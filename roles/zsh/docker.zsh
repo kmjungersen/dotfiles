afda095447dd305128b7f6acf6d09c164cdea018
Kurtis Jungersen <kmjungersen@gmail.com>
2015-10-10 20:29:27 -0400

    fixed soe git issues

--- roles/zsh/docker.zsh
+++ roles/zsh/docker.zsh
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 export DOCKER_HOST=tcp://192.168.59.104:2376
-=======
+#=======
 export DOCKER_HOST=tcp://192.168.59.103:2376
->>>>>>> 969e42b67f6023fd85396410084cd7ad72e06a51
+#>>>>>>> 969e42b67f6023fd85396410084cd7ad72e06a51
 export DOCKER_CERT_PATH=/Users/kmjungersen/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
